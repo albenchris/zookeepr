23743be926956547ee1d0265659f1077ebbe1cbe
albenchris <albenchris00@gmail.com>
2021-02-22 14:38:32 -0600

    added api functionality to animals page

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -1,3 +1,4 @@
+
 const $animalForm = document.querySelector('#animals-form');
 const $displayArea = document.querySelector('#display-area');
 
@@ -31,6 +32,17 @@ const getAnimals = (formData = {}) => {
 
   console.log(queryUrl);
 
+  fetch(queryUrl)
+    .then(response => {
+      if (!response.ok) {
+        return alert('Error: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(animalData => {
+      console.log(animalData);
+      printResults(animalData);
+    });
 };
 
 const handleGetAnimalsSubmit = event => {
