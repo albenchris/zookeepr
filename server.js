c4a8ab448b361ec34b5892ce09cb91a74aab7794
albenchris <albenchris00@gmail.com>
2021-02-22 14:17:28 -0600

    user can now add animals on main page

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// to use css and js
+app.use(express.static('public'));
 const { animals } = require('./data/animals.json')
 
 function filterByQuery(query, animalsArr) {
@@ -112,6 +114,10 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
